5c7ab44f3fbbfcbf6eba21bcce8632ca053e87c2
marvek16 <marvelousk1603f@gmail.com>
2025-07-25 08:15:48 +0200

    commit

--- chapter3/chapter3.sql
+++ chapter3/chapter3.sql
@@ -16,3 +16,33 @@ WITH (FORMAT CSV, HEADER, DELIMITER '|');
 INSERT INTO char_data_types
 VALUES
 	('1234567890', '1234567890', 'this is a text' )
+
+CREATE TABLE date_time_types (
+    timestamp_column timestamp with time zone,
+    interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+    ('2018-12-31 01:00 EST','2 days'),
+    ('2018-12-31 01:00 PST','1 month'),
+    ('2018-12-31 01:00 Australia/Melbourne','1 century'),
+    (now(),'1 week');
+
+SELECT * FROM date_time_types;
+
+CREATE TABLE date_time_types (
+    timestamp_column timestamp with time zone,
+    interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+    ('2018-12-31 01:00 EST','2 days'),
+    ('2018-12-31 01:00 PST','1 month'),
+    ('2018-12-31 01:00 Australia/Melbourne','1 century'),
+    (now(),'1 week');
+
+SELECT * FROM date_time_types;
+
+
---
+++ chapter4/chapter4.sql
@@ -0,0 +1,3 @@
+COPY teachers TO 'C:\Bootcamp\sql\mycode\chapter4\typetest.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
