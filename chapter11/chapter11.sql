2fe78dab14345e611afc73a8446829f08e910611
marvek16 <marvelousk1603f@gmail.com>
2025-08-04 08:48:51 +0200

    commit

---
+++ chapter11/chapter11.sql
@@ -0,0 +1,168 @@
+-- Listing 11-1: Extracting components of a timestamp value using date_part()
+
+SELECT
+    date_part('year', '2019-12-01 18:37:12 EST'::timestamptz) AS "year",
+    date_part('month', '2019-12-01 18:37:12 EST'::timestamptz) AS "month",
+    date_part('day', '2019-12-01 18:37:12 EST'::timestamptz) AS "day",
+    date_part('hour', '2019-12-01 18:37:12 EST'::timestamptz) AS "hour",
+    date_part('minute', '2019-12-01 18:37:12 EST'::timestamptz) AS "minute",
+    date_part('seconds', '2019-12-01 18:37:12 EST'::timestamptz) AS "seconds",
+    date_part('timezone_hour', '2019-12-01 18:37:12 EST'::timestamptz) AS "tz",
+    date_part('week', '2019-12-01 18:37:12 EST'::timestamptz) AS "week",
+    date_part('quarter', '2019-12-01 18:37:12 EST'::timestamptz) AS "quarter",
+    date_part('epoch', '2019-12-01 18:37:12 EST'::timestamptz) AS "epoch";
+
+-- Bonus: Using the SQL-standard extract() for similar datetime parsing:
+
+SELECT extract('year' from '2019-12-01 18:37:12 EST'::timestamptz) AS "year";
+
+-- Listing 11-2: Three functions for making datetimes from components
+
+-- make a date
+SELECT make_date(2018, 2, 22);
+-- make a time
+SELECT make_time(18, 4, 30.3);
+-- make a timestamp with time zone
+SELECT make_timestamptz(2018, 2, 22, 18, 4, 30.3, 'Europe/Lisbon');
+
+-- Bonus: Retrieving the current date and time
+
+SELECT
+    current_date,
+    current_time,
+    current_timestamp,
+    localtime,
+    localtimestamp,
+    now();
+
+-- Listing 11-3: Comparing current_timestamp and clock_timestamp() during row insert
+
+CREATE TABLE current_time_example (
+    time_id bigserial,
+    current_timestamp_col timestamp with time zone,
+    clock_timestamp_col timestamp with time zone
+);
+
+INSERT INTO current_time_example (current_timestamp_col, clock_timestamp_col)
+    (SELECT current_timestamp,
+            clock_timestamp()
+     FROM generate_series(1,1000));
+
+SELECT * FROM current_time_example;
+
+-- Time Zones
+
+-- Listing 11-4: Showing your PostgreSQL server's default time zone
+
+SHOW timezone; -- Note: You can see all run-time defaults with SHOW ALL;
+
+-- Listing 11-5: Showing time zone abbreviations and names
+
+SELECT * FROM pg_timezone_abbrevs;
+SELECT * FROM pg_timezone_names;
+
+-- Filter to find one
+SELECT * FROM pg_timezone_names
+WHERE name LIKE 'Europe%';
+
+-- Listing 11-6: Setting the time zone for a client session
+
+SET timezone TO 'US/Pacific';
+
+CREATE TABLE time_zone_test (
+    test_date timestamp with time zone
+);
+INSERT INTO time_zone_test VALUES ('2020-01-01 4:00');
+
+SELECT test_date
+FROM time_zone_test;
+
+SET timezone TO 'US/Eastern';
+
+SELECT test_date
+FROM time_zone_test;
+
+SELECT test_date AT TIME ZONE 'Asia/Seoul'
+FROM time_zone_test;
+
+
+-- Math with dates!
+
+SELECT '9/30/1929'::date - '9/27/1929'::date;
+SELECT '9/30/1929'::date + '5 years'::interval;
+
+
+-- Taxi Rides
+
+-- Listing 11-7: Creating a table and importing NYC yellow taxi data
+
+CREATE TABLE nyc_yellow_taxi_trips_2016_06_01 (
+    trip_id bigserial PRIMARY KEY,
+    vendor_id varchar(1) NOT NULL,
+    tpep_pickup_datetime timestamp with time zone NOT NULL,
+    tpep_dropoff_datetime timestamp with time zone NOT NULL,
+    passenger_count integer NOT NULL,
+    trip_distance numeric(8,2) NOT NULL,
+    pickup_longitude numeric(18,15) NOT NULL,
+    pickup_latitude numeric(18,15) NOT NULL,
+    rate_code_id varchar(2) NOT NULL,
+    store_and_fwd_flag varchar(1) NOT NULL,
+    dropoff_longitude numeric(18,15) NOT NULL,
+    dropoff_latitude numeric(18,15) NOT NULL,
+    payment_type varchar(1) NOT NULL,
+    fare_amount numeric(9,2) NOT NULL,
+    extra numeric(9,2) NOT NULL,
+    mta_tax numeric(5,2) NOT NULL,
+    tip_amount numeric(9,2) NOT NULL,
+    tolls_amount numeric(9,2) NOT NULL,
+    improvement_surcharge numeric(9,2) NOT NULL,
+    total_amount numeric(9,2) NOT NULL
+);
+
+COPY nyc_yellow_taxi_trips_2016_06_01 (
+    vendor_id,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    pickup_longitude,
+    pickup_latitude,
+    rate_code_id,
+    store_and_fwd_flag,
+    dropoff_longitude,
+    dropoff_latitude,
+    payment_type,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    improvement_surcharge,
+    total_amount
+   )
+FROM 'C:\Bootcamp\sql\practical-sql-main\Chapter_11\yellow_tripdata_2016_06_01.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+
+CREATE INDEX tpep_pickup_idx
+ON nyc_yellow_taxi_trips_2016_06_01 (tpep_pickup_datetime);
+
+SELECT count(*) FROM nyc_yellow_taxi_trips_2016_06_01;
+
+-- Listing 11-8: Counting taxi trips by hour
+
+SELECT
+    date_part('hour', tpep_pickup_datetime) AS trip_hour,
+    count(*)
+FROM nyc_yellow_taxi_trips_2016_06_01
+GROUP BY trip_hour
+ORDER BY trip_hour;
+
+SET timezone TO 'Africa/Johannesburg';
+
+SELECT
+ date_part('hour', tpep_pickup_datetime) AS trip_hour,
+ count(*)
+FROM nyc_yellow_taxi_trips_2016_06_01
+GROUP BY trip_hour 
+ORDER BY count(*) DESC LIMIT 1;
---
+++ excersise/excersise.sql
@@ -0,0 +1,208 @@
+CREATE TABLE books (
+	book_name varchar(50), 
+	author_name varchar(50)  
+);
+
+SELECT * FROM books;
+
+INSERT INTO books (book_name, author_name)
+VALUES 
+	('Iron Flame', 'Rebecca Yarros' );
+
+ALTER TABLE books ADD COLUMN pages integer;
+
+INSERT INTO books (pages)
+VALUES 
+	(365);
+
+UPDATE books 
+SET pages = 365
+
+
+--didn't work
+DELETE FROM books
+WHERE id = 2;
+
+--right method to delete a row
+DELETE FROM books
+WHERE book_name IS NULL AND author_name IS NULL AND pages = 365;
+
+
+CREATE TABLE department (
+    department_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    facility VARCHAR(100)
+);
+
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR (100) UNIQUE,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO department (name, facility)
+VALUES
+    ('SQL', 'Home'),
+    ('MERN', 'Office');
+
+ALTER TABLE employees
+ADD COLUMN department_id integer,
+ADD CONSTRAINT fk_department
+FOREIGN KEY (department_id)
+REFERENCES department(department_id);
+
+
+INSERT INTO employees (full_name, email, department_id) 
+VALUES
+	('Alexander Badenhorst', 'alex@example.com', 1),
+	('Courtney Cook', 'courtsey_of_courtney@funmail.io', 2),
+	('Cadee Rousseau', 'codee.withcadee@byte.com', 1),
+	('David Koen', 'teachinator.david@brainboost.edu', 1),
+	('Ethan Hurwitz', 'ethanator@hackmail.com', 1),
+	('Lindokuhle Yende', 'lindo.logic@neuralhub.org', 1),
+	('Marvelous Kahundahunda', 'absolutely.marvelous@wow.com', 1),
+	('Pierre Kahundahunda', 'pierrefect.code@bonjour.dev', 1),
+	('Ronny Mputla', 'run.run.ronny@speedmail.net', 1),
+	('Sibusiso Makhiwane', 'sibu.saves@galaxymail.com', 1),
+	('Tom Vuma', 'tom.tornado@scriptstorm.dev', 1),
+	('Ulrich Snyman', 'ultimatecoding@master.com', 1);
+
+SELECT * FROM department
+SELECT * FROM employees
+
+COPY (
+    SELECT 
+        e.full_name, 
+        e.email, 
+        d.name AS department_name
+    FROM employees e
+    JOIN department d ON e.department_id = d.department_id
+) TO 'C:\Bootcamp\sql\mycode\excersise\joined_employees_departments.csv' CSV HEADER;
+
+SELECT 
+    e.employee_id,
+    e.full_name,
+    e.email,
+    d.name AS department_name,
+    d.facility
+FROM 
+    employees e
+JOIN 
+    department d
+ON 
+    e.department_id = d.department_id;
+
+ALTER TABLE employees
+ADD COLUMN score INTEGER;
+
+UPDATE employees SET score = 78 WHERE full_name = 'Alexander Badenhorst';
+UPDATE employees SET score = 81 WHERE full_name = 'Cadee Rousseau';
+UPDATE employees SET score = 85 WHERE full_name = 'David Koen';
+UPDATE employees SET score = 76 WHERE full_name = 'Ethan Hurwitz';
+UPDATE employees SET score = 94 WHERE full_name = 'Lindokuhle Yende';
+UPDATE employees SET score = 89 WHERE full_name = 'Marvelous Kahundahunda';
+UPDATE employees SET score = 92 WHERE full_name = 'Pierre Kahundahunda';
+UPDATE employees SET score = 74 WHERE full_name = 'Ronny Mputla';
+UPDATE employees SET score = 75 WHERE full_name = 'Sibusiso Makhiwane';
+UPDATE employees SET score = 80 WHERE full_name = 'Tom Vuma';
+UPDATE employees SET score = 87 WHERE full_name = 'Ulrich Snyman';
+UPDATE employees SET score = 65 WHERE full_name = 'Courtney Cook';
+
+SELECT 
+    percentile_cont(.9) 
+    WITHIN GROUP (ORDER BY score) AS percentile_90_score
+FROM employees;
+
+SELECT full_name, score
+FROM employees
+WHERE score = (
+    SELECT MAX(score) FROM employees
+);
+
+SELECT full_name, score
+FROM employees
+WHERE score = (SELECT MIN(score) FROM employees);
+
+
+SELECT full_name, score
+FROM employees
+WHERE score = (
+    SELECT ROUND(AVG(score)) FROM employees
+);
+
+
+--SELECT full_name, score
+--FROM employees
+--ORDER BY score DESC
+--LIMIT 3;
+
+
+--Challenge
+
+-- Step 1: Create roles table
+CREATE TABLE roles (
+	role_id BIGSERIAL PRIMARY KEY,
+	role_name VARCHAR(50),
+	role_description TEXT,
+	salaries INTEGER
+);
+
+-- Step 2: Create employees2 table (remove role_name column)
+CREATE TABLE employees2 (
+	emp_id BIGSERIAL PRIMARY KEY,
+	emp_name VARCHAR(50),
+	birthdate DATE,
+	role_id BIGINT REFERENCES roles(role_id),
+	sales INTEGER
+);
+
+-- Step 3: Insert role data
+INSERT INTO roles (role_name, role_description, salaries)
+VALUES 
+('Graphic Designer', 'Helps with video editing, photo editing and market advertisement designs', 35000),
+('Videographer', 'Helps with all digital media productions', 18000),
+('Social Marketer', 'Helps with social media strategies and social data', 26000),
+('Sales Rep', 'Helps promote and sign clients', 20000);
+
+-- Step 4: View roles (optional)
+SELECT * FROM employees2;
+
+-- Step 5: Insert employee data (using correct role_id)
+-- Make sure role_id values match the auto-generated IDs from the roles table
+-- Assuming: 1 = Graphic Designer, 2 = Videographer, 3 = Social Marketer, 4 = Sales Rep
+INSERT INTO employees2 (emp_name, birthdate, role_id, sales)
+VALUES 
+('Aragorn', '1994-08-24', 4, 22),
+('Gandalf', '1982-05-11', 1, 0),
+('Frodo', '1990-01-18', 2, 0),
+('Legolas', '1998-04-22', 3, 0),
+('Gimli', '2000-11-08', 4, 10),
+('Samwise', '2001-01-01', 4, 9),
+('Pippin', '1999-09-26', 4, 18),
+('Merry', '2005-08-07', 3, 0);
+
+DROP TABLE roles;
+DROP TABLE employees2;
+
+	SELECT 
+    e.emp_name,
+    e.birthdate,
+    r.role_name,
+    r.salaries AS base_salary,
+    e.sales,
+    CASE 
+        WHEN r.role_name = 'Sales Rep' AND e.sales > 10 THEN r.salaries + (e.sales - 10) * 200
+        ELSE r.salaries
+    END AS adjusted_salary
+FROM 
+    employees2 e
+JOIN 
+    roles r ON e.role_id = r.role_id
+
+SELECT *
+FROM employees2 e
+WHERE AGE(CURRENT_DATE, e.birthdate) >= INTERVAL '27 years';
+
+
+
